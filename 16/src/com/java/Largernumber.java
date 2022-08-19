8b6784bb5a5cb8b25afd8e4ae58548e5e6efe50c
Damelamanroi <111557226+Damelamanroi@users.noreply.github.com>
2022-08-19 12:07:44 +0530

    Add files via upload

---
+++ 16/src/com/java/Largernumber.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Largernumber {
+	public static void main(String args[]) {
+		int x=150, y=100;
+		if(x<y) {
+			System.out.println("The larger number is :" +y);
+		}
+		else {
+			System.out.println("The larger number is : " +x);
+		}
+	}
+}
