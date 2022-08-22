c4c881829a1203d00cc9f5600fbe967b3124facf
Damelamanroi <111557226+Damelamanroi@users.noreply.github.com>
2022-08-22 11:23:48 +0530

    Add files via upload

---
+++ 27/src/com/java/Input.java
@@ -0,0 +1,21 @@
+package com.java;
+
+import java.util.Scanner;
+public class Input {
+	public static void main(String args[]) {
+	
+		for(int x=10; x>0; x--) {
+			if(x%2==0) {
+			System.out.println("The even number from 1 to 10 :" );
+			System.out.println(+x);}
+			else {
+				System.out.println("not defined");
+			}
+		}
+	
+	
+	
+	}
+
+}
+
