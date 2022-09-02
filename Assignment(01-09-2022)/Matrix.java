53b2d1aba270d1b26755e5e02113b3564cbf4026
Damelamanroi <111557226+Damelamanroi@users.noreply.github.com>
2022-09-02 14:23:14 +0530

    Update Matrix.java

--- Assignment(01-09-2022)/Matrix.java
+++ Assignment(01-09-2022)/Matrix.java
@@ -2,12 +2,15 @@ package com.java;
 /*WAP to input an array having 3 rows and 3 columns of type String*/
 import java.util.Arrays;
 public class Matrix {
-	public static void main(String[] args) {
-		int[][] matrix= {{1,2,3},{4,5,6}};
-		for(int i=0; i<3;i++) {
-			for(int j=0; j<3; j++) {
-				System.out.println("[" + i + "] [" + j + "] = " +matrix[i][j] );
-			}
-		}
+		int[][] intArray = { { 1, 2,3 }, { 4,5,6 },{7,8,9}}; 
+        
+        System.out.println("Display the  array:");
+        for (int i = 0; i < 3; i++) { 
+            for (int j = 0; j < 3; j++) { 
+            System.out.print(intArray [i][j] + " "); 
+            } 
+ 
+            System.out.println(); 
+        } 
 	} 
 }
